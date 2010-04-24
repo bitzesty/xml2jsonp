9b203457ac75961932b8978904139713cbcbe22d
Bit Zesty <info@bitzesty.com>
2010-04-24 18:25:41 +0100

    add attribution link

--- xml2json.rb
+++ xml2json.rb
@@ -10,6 +10,6 @@ get '/' do
     xml = Crack::XML.parse(resp)
     "#{params['callback']}(#{xml.to_json})" 
   else
-    "<h1>XML2JSONP API Proxy</h1><code>required params['url'] and params['callback']</code><p><a href='http://github.com/bitzesty/xml2jsonp'>http://github.com/bitzesty/xml2jsonp</a></p>"
+    "<h1>XML2JSONP API Proxy</h1><code>required params['url'] and params['callback']</code><p><a href='http://github.com/bitzesty/xml2jsonp'>http://github.com/bitzesty/xml2jsonp</a> by <a href='http://bitzesty.com'>Bit Zesty - a Ruby on Rails development company</a></p>"
   end
 end
